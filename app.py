b0a87425fdf3e245c588a4f3ef72052c944e2a1a
Dmitry Andreev <dmitry.andreev@latoken.com>
2019-11-07 22:43:24 +0300

    setup for heroku

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from werkzeug.exceptions import BadRequest, NotFound
 from short_url import create_db, create_short_url, get_full_url, create_short_url
 
 app = Flask(__name__)
+create_db()
 
 
 @app.route("/")
@@ -36,5 +37,4 @@ def full(short_url):
 
 
 if __name__ == "__main__":
-    create_db()
     app.run()
--- short_url.py
+++ short_url.py
@@ -1,12 +1,14 @@
-import sqlite3
+import psycopg2
 import base62
+import os
 
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 URLS_DB = "short_urls.db"
 
 
 def create_db():
-    with sqlite3.connect(URLS_DB) as conn:
+    with psycopg2.connect(DSN) as conn:
         c = conn.cursor()
         c.execute(
             "CREATE TABLE IF NOT EXISTS urls (id INTEGER NOT NULL PRIMARY KEY, url TEXT)"
@@ -14,7 +16,7 @@ def create_db():
 
 
 def get_short_url(url):
-    with sqlite3.connect(URLS_DB) as conn:
+    with psycopg2.connect(DSN) as conn:
         c = conn.cursor()
         id_ = c.execute("SELECT id FROM urls WHERE url=?", (url,)).fetchone()
     if id_:
@@ -28,7 +30,7 @@ def get_full_url(url):
     except ValueError:
         return
 
-    with sqlite3.connect(URLS_DB) as conn:
+    with psycopg2.connect(DSN) as conn:
         c = conn.cursor()
         url = c.execute("SELECT url FROM urls WHERE id=?", (id_,)).fetchone()
     if url:
@@ -40,7 +42,7 @@ def create_short_url(url):
     if short_url:
         return short_url
 
-    with sqlite3.connect(URLS_DB) as conn:
+    with psycopg2.connect(DSN) as conn:
         c = conn.cursor()
         num_of_urls = c.execute("SELECT COUNT(*) FROM urls").fetchone()[0]
         c.execute("INSERT INTO urls (id, url) VALUES(?, ?);", (num_of_urls, url))
